8f122601505081c29ac025b5d920cc0def1b8cd9
George Tsomlektsis <ultrawelfaredev@gmail.com>
2024-11-21 20:35:05 +0200

    fix wpf template accidentally removing conditionals. removed unused imports.
    fixed template copy targets having the same name

--- PhosphorusNET.Templates/WpfApp/MainWindow.xaml.cs
+++ PhosphorusNET.Templates/WpfApp/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Microsoft.Web.WebView2.Core;
 using PhosphorusNET.Ipc;
 using WpfApp.Ipc;
 
@@ -23,18 +15,20 @@ public partial class MainWindow : Window
         InitializeComponent();
         OnLoad();
     }
-    
+
     private async void OnLoad()
     {
         webView.CoreWebView2InitializationCompleted += (_, _) =>
         {
+            //-:cnd:noEmit
 #if RELEASE
-        webView.CoreWebView2.SetVirtualHostNameToFolderMapping("app", "wwwroot",
-            CoreWebView2HostResourceAccessKind.Allow);
-        webView.CoreWebView2.Navigate("https://app/index.html");
+            webView.CoreWebView2.SetVirtualHostNameToFolderMapping("app", "wwwroot",
+                CoreWebView2HostResourceAccessKind.Allow);
+            webView.CoreWebView2.Navigate("https://app/index.html");
 #else
             webView.CoreWebView2.Navigate("http://localhost:5173");
 #endif
+            //+:cnd:noEmit
         };
 
         await webView.EnsureCoreWebView2Async();
